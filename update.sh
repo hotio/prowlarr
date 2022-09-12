9c2169d02144403afcbb46869288425d0796749d
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-12 12:46:11 +0200

    skip ci

--- update.sh
+++ update.sh
@@ -30,5 +30,6 @@ else
     version=$(curl -fsSL "https://prowlarr.servarr.com/v1/update/develop/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -r .[0].version)
     [[ -z ${version} ]] && exit 1
     [[ ${version} == "null" ]] && exit 0
-    echo '{"version":"'"${version}"'"}' | jq . > VERSION.json
+    version_json=$(cat ./VERSION.json)
+    jq '.version = "'"${version}"'"' <<< "${version_json}" > VERSION.json
 fi
