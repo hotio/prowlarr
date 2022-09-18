34c23e0d5c34de0c8a736f7298b91f51c29762c8
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-18 07:36:02 +0200

    fix

--- update-versions.sh
+++ update-versions.sh
@@ -5,4 +5,4 @@ version=$(curl -fsSL "https://prowlarr.servarr.com/v1/update/${branch}/changes?o
 [[ -z ${version} ]] && exit 0
 [[ ${version} == "null" ]] && exit 0
 version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .branch = "'"${branch}"'"' <<< "${version_json}" > VERSION.json
+jq '.version = "'"${version}"'" | .sbranch = "'"${branch}"'"' <<< "${version_json}" > VERSION.json
