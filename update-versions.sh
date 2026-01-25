65b054de00f94554b3ee109ad6cfa5d51e3cb229
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-25 03:46:29 +0100

    add auth [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -exuo pipefail
 
-version_branch=$(curl -fsSL "https://api.github.com/repos/prowlarr/prowlarr/pulls?state=open&sort=updated&direction=desc" | jq -re '[.[] | select((.head.repo.full_name == "Prowlarr/Prowlarr") and (.head.ref | contains("dependabot") | not) and (.base.ref == "develop")) | .head.ref][0]')
+version_branch=$(curl -fsSL --header "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/prowlarr/prowlarr/pulls?state=open&sort=updated&direction=desc" | jq -re '[.[] | select((.head.repo.full_name == "Prowlarr/Prowlarr") and (.head.ref | contains("dependabot") | not) and (.base.ref == "develop")) | .head.ref][0]')
 version=$(curl -fsSL "https://prowlarr.servarr.com/v1/update/${version_branch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -re '.[0].version')
 curl -fsSL "https://prowlarr.servarr.com/v1/update/${version_branch}/updatefile?version=${version}&os=linuxmusl&runtime=netcore&arch=x64" -o /dev/null
 json=$(cat meta.json)
